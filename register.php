3ac371ed7ca727f417b4c4d2834cba5baf02da54
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-09-22 18:34:40 -0300

    Feito a validação para email único no register.php, utilizando o config.php para a conexão com o banco de dados. Um if para caso o email preenchido já exista, exibir uma mensagem para utilizar outro email(também fiz um botão para voltar) e um else para caso não seja o caso, mostrar uma mensagem de registro feito com sucesso e um botão para fazer o login após o cadastro.

--- register.php
+++ register.php
@@ -11,6 +11,38 @@
  <body>
         <div class="container">
             <div class="box form-box"></div>
+
+         <?php 
+         
+         include("php/config.php");
+         if(isset($_POST['submit'])){
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $age = $_POST['age'];
+            $password = $_POST['password'];
+
+         $verify_query = mysqli_query($con,"SELECT Email FROM users WHERE Email='$email'");
+
+         if(mysqli_num_rows($verify_query) !=0 ){
+            echo "<div class='message'>
+                      <p>Esse email já foi usado, tente outro porfavor!</p>
+                  </div> <br>";
+            echo "<a href='javascript:self.history.back()'><button class='btn'>Volte</button>";
+         }
+         else{
+
+            mysqli_query($con,"INSERT INTO users(Username,Email,Age,Password) VALUES('$username','$email','$age','$password')") or die("Um erro ocorreu");
+
+            echo "<div class='message'>
+                      <p>Conta registrada com sucesso!</p>
+                  </div> <br>";
+            echo "<a href='index.php'><button class='btn'>Faça login agora</button>";
+         }
+
+         }else{
+
+        ?>
+
                 <header>Entrar com uma conta</header>
                 <form action="" method="post">
                     <div class="field input">
@@ -35,12 +67,13 @@
                     
                     <div class="field">
                     
-                        <input type="submit" name="submit" value="Login" required>
+                        <input type="submit" name="submit" value="Register" required>
                     </div>
                     <div class="links">
                         Já é um membro? <a href="registrer.html">Entrar</a>
                     </div>
                 </form>
+                <?php } ?>
         </div>
     
  </body>
