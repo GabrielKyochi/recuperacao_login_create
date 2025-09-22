fb9ef62485febf6b10122751a533c25bb38204be
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-09-22 19:18:36 -0300

    Arrumado o edit.php e register.php.

--- edit.php
+++ edit.php
@@ -1,3 +1,11 @@
+<?php 
+   session_start();
+
+   include("php/config.php");
+   if(!isset($_SESSION['valid'])){
+    header("Location: index.php");
+   }
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -5,22 +13,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-    <title>Editar perfil</title>
+    <title>Change Profile</title>
 </head>
 <body>
-     <div class="nav">
+    <div class="nav">
         <div class="logo">
             <p><a href="home.php"> Logo</a></p>
         </div>
 
         <div class="right-links">
-            <a href='#'>Trocar perfil</a>";
+            <a href="#">Alterar Perfil</a>
             <a href="php/logout.php"> <button class="btn">Sair</button> </a>
         </div>
     </div>
     <div class="container">
-    <div class="box form-box"></div>
-     <?php 
+        <div class="box form-box">
+            <?php 
                if(isset($_POST['submit'])){
                 $username = $_POST['username'];
                 $email = $_POST['email'];
@@ -34,7 +42,7 @@
                     echo "<div class='message'>
                     <p>Perfil Atualizado!</p>
                 </div> <br>";
-              echo "<a href='home.php'><button class='btn'>Vá para a aba home!</button>";
+              echo "<a href='home.php'><button class='btn'>Vá para a aba home</button>";
        
                 }
                }else{
@@ -47,39 +55,33 @@
                     $res_Email = $result['Email'];
                     $res_Age = $result['Age'];
                 }
+
             ?>
-                <header>Entrar com uma conta</header>
-                <form action="" method="post">
-                    <div class="field input">
-                        <label for="username">Username</label>   
-                        <input type="text" name="username" id="username" autocomplete="off" required>
-                    </div>
+            <header>Trocar Perfil</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="username">Nome de usuário</label>
+                    <input type="text" name="username" id="username" value="<?php echo $res_Uname; ?>" autocomplete="off" required>
+                </div>
 
-                    <div class="field input">
-                        <label for="email">Email</label>   
-                        <input type="text" name="email" id="email" autocomplete="off" required>
-                    </div>
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" value="<?php echo $res_Email; ?>" autocomplete="off" required>
+                </div>
 
-                    <div class="field input">
-                        <label for="age">Age</label>   
-                        <input type="number" name="age" id="age" autocomplete="off" required>
-                    </div>
-
-                    <div class="field input">
-                        <label for="password">Password</label>   
-                        <input type="password" name="password" id="password" autocomplete="off" required>
-                    </div>
+                <div class="field input">
+                    <label for="age">Idade</label>
+                    <input type="number" name="age" id="age" value="<?php echo $res_Age; ?>" autocomplete="off" required>
+                </div>
+                
+                <div class="field">
                     
-                    <div class="field">
-                    
-                        <input type="submit" name="submit" value="Login" required>
-                    </div>
-                    <div class="links">
-                        Já é um membro? <a href="index.php">Entrar</a>
-                    </div>
-                </form>
+                    <input type="submit" class="btn" name="submit" value="Update" required>
+                </div>
+                
+            </form>
         </div>
-        <?php } ?> 
-    </div>
+        <?php } ?>
+      </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,18 +1,17 @@
- <html lang="pt-BR">
- <head>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
     <title>Register</title>
+</head>
+<body>
+      <div class="container">
+        <div class="box form-box">
 
-    <link rel="stylesheet" href="style/style.css">
- </head>
- <body>
-        <div class="container">
-            <div class="box form-box"></div>
-
-         <?php 
+        <?php 
          
          include("php/config.php");
          if(isset($_POST['submit'])){
@@ -21,60 +20,63 @@
             $age = $_POST['age'];
             $password = $_POST['password'];
 
+            //Verificando o email único
+
          $verify_query = mysqli_query($con,"SELECT Email FROM users WHERE Email='$email'");
 
          if(mysqli_num_rows($verify_query) !=0 ){
             echo "<div class='message'>
-                      <p>Esse email já foi usado, tente outro porfavor!</p>
+                      <p>Esse email já está em uso, tente outro porfavor!!</p>
                   </div> <br>";
             echo "<a href='javascript:self.history.back()'><button class='btn'>Volte</button>";
          }
          else{
 
-            mysqli_query($con,"INSERT INTO users(Username,Email,Age,Password) VALUES('$username','$email','$age','$password')") or die("Um erro ocorreu");
+            mysqli_query($con,"INSERT INTO users(Username,Email,Age,Password) VALUES('$username','$email','$age','$password')") or die("Erroe Occured");
 
             echo "<div class='message'>
-                      <p>Conta registrada com sucesso!</p>
+                      <p>Registrado com sucesso!</p>
                   </div> <br>";
-            echo "<a href='index.php'><button class='btn'>Faça login agora</button>";
+            echo "<a href='index.php'><button class='btn'>Entre agora</button>";
+         
+
          }
 
          }else{
-
+         
         ?>
 
-                <header>Entrar com uma conta</header>
-                <form action="" method="post">
-                    <div class="field input">
-                        <label for="username">Username</label>   
-                        <input type="text" name="username" id="username" autocomplete="off" required>
-                    </div>
+            <header>Entre</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="username">Nome de usuário</label>
+                    <input type="text" name="username" id="username" autocomplete="off" required>
+                </div>
 
-                    <div class="field input">
-                        <label for="email">Email</label>   
-                        <input type="text" name="email" id="email" autocomplete="off" required>
-                    </div>
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" autocomplete="off" required>
+                </div>
 
-                    <div class="field input">
-                        <label for="age">Age</label>   
-                        <input type="number" name="age" id="age" autocomplete="off" required>
-                    </div>
+                <div class="field input">
+                    <label for="age">Idade</label>
+                    <input type="number" name="age" id="age" autocomplete="off" required>
+                </div>
+                <div class="field input">
+                    <label for="password">Senha</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
 
-                    <div class="field input">
-                        <label for="password">Password</label>   
-                        <input type="password" name="password" id="password" autocomplete="off" required>
-                    </div>
+                <div class="field">
                     
-                    <div class="field">
-                    
-                        <input type="submit" name="submit" value="Register" required>
-                    </div>
-                    <div class="links">
-                        Já é um membro? <a href="registrer.html">Entrar</a>
-                    </div>
-                </form>
-                <?php } ?>
+                    <input type="submit" class="btn" name="submit" value="Register" required>
+                </div>
+                <div class="links">
+                    Já é um membro? <a href="index.php">Entre</a>
+                </div>
+            </form>
         </div>
-    
- </body>
- </html>
+        <?php } ?>
+      </div>
+</body>
+</html>
