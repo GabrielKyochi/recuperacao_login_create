fd6d020cc1c40e01a902bc2854f36cf66652537f
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-09-22 18:46:07 -0300

    Feito a validação para os casos de nome ou senha errados durante o login do usuário. Além disso, realizei uma mensagem para caso isso aconteça e fiz um botão de link que volta para a página anterior, por último, fiz um if para caso o usuário consiga preencher todas as informações corretamente, levar para a página de home,

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
- <html lang="pt-BR">
+<?php 
+   session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,15 +13,45 @@
  <body>
         <div class="container">
             <div class="box form-box"></div>
+            
+            <?php 
+              include("php/config.php");
+              if(isset($_POST['submit'])){
+                $email = mysqli_real_escape_string($con,$_POST['email']);
+                $password = mysqli_real_escape_string($con,$_POST['password']);
+
+                $result = mysqli_query($con,"SELECT * FROM users WHERE Email='$email' AND Password='$password' ") or die("Selecionar Erro");
+                $row = mysqli_fetch_assoc($result);
+
+                if(is_array($row) && !empty($row)){
+                    $_SESSION['valid'] = $row['Email'];
+                    $_SESSION['username'] = $row['Username'];
+                    $_SESSION['age'] = $row['Age'];
+                    $_SESSION['id'] = $row['Id'];
+                }else{
+                    echo "<div class='message'>
+                      <p>Nome ou senha errados</p>
+                       </div> <br>";
+                   echo "<a href='index.php'><button class='btn'>Volte</button>";
+         
+                }
+                if(isset($_SESSION['valid'])){
+                    header("Location: home.php");
+                }
+              }else{
+
+            
+            ?>
+            
                 <header>Login</header>
                 <form action="" method="post">
                     <div class="field input">
-                        <label for="username">Username</label>   
-                        <input type="text" name="username" id="username" required>
+                        <label for="email">Email</label>   
+                        <input type="text" name="email" id="email" required>
                     </div>
 
                     <div class="field input">
-                        <label for="password">Password</label>   
+                        <label for="password">Senha</label>   
                         <input type="password" name="password" id="password" required>
                     </div>
                     
@@ -29,6 +63,7 @@
                         Você não tem uma conta? <a href="register.php">Criar uma conta agora</a>
                     </div>
                 </form>
+            <?php } ?>
         </div>
     
  </body>
